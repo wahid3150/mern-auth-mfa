86b0439791bd4b71ccf4fc26505f9ee9f42101e6
Wahid ali <wahidali3150@gmail.com>
2026-01-29 01:17:18 +0500

    Basic express server are created

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
