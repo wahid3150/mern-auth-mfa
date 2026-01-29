eee33589770786aa828725eecf152011d671abaf
Wahid ali <wahidali3150@gmail.com>
2026-01-29 15:22:29 +0500

    User model are created & Field Validation are applied through zod

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+export default User = mongoose.model("User", userSchema);
---
+++ backend/validation/zod.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 characters long"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
