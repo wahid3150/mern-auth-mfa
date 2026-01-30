c6fe0e3d0e54cd0d282d421e7a462b22737bbae3
Wahid ali <wahidali3150@gmail.com>
2026-01-30 12:47:43 +0500

    Email verification sending logic are implemented in register controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,12 @@
 import TryCatch from "../middlewares/TryCatch.js";
 import sanitize from "mongo-sanitize";
 import { registerSchema } from "../validation/zod.js";
+import { redisClient } from "../server.js";
+import { User } from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import crypto, { hash } from "crypto";
+import sendMail from "../verifyEmail/sendMail.js";
+import { getVerifyEmailHtml } from "../verifyEmail/verificationMailTemplate.js";
 
 export const registerUser = TryCatch(async (req, res) => {
   const sanitizedBody = sanitize(req.body);
@@ -28,9 +34,38 @@ export const registerUser = TryCatch(async (req, res) => {
 
   const { name, email, password } = validation.data;
 
-  res.json({
+  const rateLimitKey = `register-rate-limit:${req.ip}:${email}`;
+  if (await redisClient.get(rateLimitKey)) {
+    return res.status(429).json({
+      message: "Too many request, try again later",
+    });
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const verifyToken = crypto.randomBytes(32).toString("hex");
+  const verifyKey = `verify:${verifyToken}`;
+  const dataToStore = JSON.stringify({
     name,
     email,
-    password,
+    password: hashedPassword,
+  });
+
+  await redisClient.set(verifyKey, dataToStore, { EX: 300 });
+  const subject = "Verify your email for account creation";
+  const html = getVerifyEmailHtml({ email, token: verifyToken });
+
+  await sendMail({ email, subject, html });
+
+  await redisClient.set(rateLimitKey, "true", { EX: 60 });
+  res.json({
+    message:
+      "If your email is valid, a verification link has been sent. It will be expire in 5 minutes",
   });
 });
