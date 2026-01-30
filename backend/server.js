3759ecf2a28391764b0f4988474839bf56296ee9
Wahid ali <wahidali3150@gmail.com>
2026-01-30 12:46:01 +0500

    Redis database connection are created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
+import { createClient } from "redis";
 
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 connectDB();
+
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  console.log("Missing redis url");
+  process.exit(1);
+}
+
+export const redisClient = createClient({
+  url: redisUrl,
+});
+redisClient
+  .connect()
+  .then(() => console.log("Connected to redis"))
+  .catch(console.error);
 
 //middleware
 app.use(express.json());
