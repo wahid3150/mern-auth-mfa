cecd217ecfe15a4baaad940aa42ae3097dc0849f
Wahid ali <wahidali3150@gmail.com>
2026-03-06 22:04:53 +0500

    Pages are imported and made routes for each

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import VerifyOtp from "./pages/VerifyOtp";
 
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verifyotp" element={<VerifyOtp />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
