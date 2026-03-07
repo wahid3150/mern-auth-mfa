caa9efc63f6c55de42a141a4a1064bd2811748f0
Wahid ali <wahidali3150@gmail.com>
2026-03-07 20:59:01 +0500

    Logout button are made in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { AppData } from "../context/AuthContext";
 
 const Home = () => {
-  return <div>HOME</div>;
+  const { logoutUser } = AppData();
+  return (
+    <div className="flex w-25 m-auto mt-40">
+      <button
+        className="bg-red-500 text-white p-2 rounded-md"
+        onClick={logoutUser}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
 };
 
 export default Home;
