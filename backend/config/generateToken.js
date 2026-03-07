6709b1a93b9a67d8ee7d45345114e279ab191048
Wahid ali <wahidali3150@gmail.com>
2026-03-07 15:43:22 +0500

    Same site security updated to none for testing

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -16,8 +16,8 @@ export const generateToken = async (id, res) => {
 
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    // secure: true,
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 1 * 60 * 1000,
   });
 
@@ -25,7 +25,7 @@ export const generateToken = async (id, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "none",
-    // secure: true
+    secure: true,
   });
 
   return { accessToken, refreshToken };
